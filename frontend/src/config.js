6062578185d1acfce1897bf781402927d4b77ef2
Rubesh <rubeshclass@gmail.com>
2026-09-12 13:57:57 +0530

    Edit in fetch link

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://cod-maternity-sporty.ngrok-free.dev';
+export const API_BASE_URL = 'https://untrue-enforcer-gauze.ngrok-free.dev';
 export const DETECTIONS_ENDPOINT = `${API_BASE_URL}/api/detections`;
